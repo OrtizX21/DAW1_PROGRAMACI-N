73dcbc957f3f90a2415b8ae7d1c87029424b0345
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-01-09 12:24:04 +0100

    Update Campeonato.java

--- Arrays/Campeonato.java
+++ Arrays/Campeonato.java
@@ -1,16 +1,32 @@
-package Arrays;
-import java.util.*;
+package Pruebas;
 
-public class Campeonato {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Campeonato_Prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		
+		/*
+		 * Diseñar una aplicación para gestionar un campeonato de programación, donde se
+		 * introduce la puntuación (enteros) obtenidos por 5 programadores, conforme van
+		 * terminando su prueba. La aplicación debe mostrar las puntuaciones ordenadas
+		 * de los 5 participantes. En ocasiones, cuando finalizan los 5 participantes
+		 * anteriores, se suman al campeonato programadores de exhibición, cuyos puntos
+		 * se incluyen con el resto. La forma de especificar que no intervienen más
+		 * programadores de exhibición es introducir como puntuación un -1. La
+		 * aplicación debe mostrar, finalmente, los puntos ordenados de todos los
+		 * participantes
+		 */
 		Scanner Puntuaciones = new Scanner (System.in);
 		
 		System.out.println("Introduce La Puntuación De Los Programadores:");
 		
 		int valores = 0;
+		
+		int valoresExhibicion = 0;
 		
 		int Programadores[] = new int [5];
 		
@@ -21,22 +37,36 @@ public class Campeonato {
 						
 			System.out.println(Arrays.toString(Programadores));
 			
+			
+			
 		}
 		
 		Arrays.sort(Programadores);
 		
 		System.out.println(Arrays.toString(Programadores));
 				
-		System.out.println(valores);
+		System.out.println("Estos son los valores: " + valores);
 		
-		while (valores != -1) {
-			
-			
-		}
 		
+		
+		  while (valores != -1) {
+			  
+			  if (valores == -1) { 
+				  
+				  System.out.println("Fin Del Programa");
+				  System.exit(0);
+			  }
+			 
+			  
+			Puntuaciones.nextInt();
 
+			
+			int ProgramadoresExhibicion [] = new int [Programadores.length];
 
-
+			System.arraycopy(Programadores, valores, ProgramadoresExhibicion, 0, Programadores.length);
+			
+		  
+		 }
 	}
 
 }
