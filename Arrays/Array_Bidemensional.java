a3c589f2b516d512e86fae0b29904116b6db20c4
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-01-25 15:32:01 +0100

    Update Array_Bidemensional.java

--- Arrays/Array_Bidemensional.java
+++ Arrays/Array_Bidemensional.java
@@ -1,43 +1,46 @@
 package Arrays;
-import java.util.*;
+import java.util.Arrays;
 
 public class Array_Bidemensional {
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		/*
-		 * Crear una tabla bidimensional de longitud 5x6 y rellenarla de la siguiente
-		 * forma: El elemento de la posición [n][m] debe contener el valor 10 x n + m.
-		 * Después se debe mostrar su contenido
-		 */		
-		
-		int [][] matriz = new int[5][6];
+
+		int[][] matriz = new int[5][4];
+
+		for (int n = 0; n < matriz.length; n++) {
+
+			System.out.println();
+
+			for (int m = 0; m < matriz[0].length; m++) {
+
+				matriz[n][m] = (10 * n + m);
+
+				System.out.print(matriz[n][m] + "\t");
+			}
+
+			System.out.print("\n");
+
+		}
 
 		
-		for (int n = 0; n < matriz.length; n++) {
-			
-			System.out.println();
-			
-			for (int m = 0; m < matriz[0].length ; m++) {
-				
-				matriz [n][m] = (10 * n + m);
-			}
-		}
-	
+		System.out.println();
+		System.out.println();
+
 		System.out.print(Arrays.deepToString(matriz));
+
 		
-		
-		for (int fila [] : matriz) {
-			
+		for (int fila[] : matriz) {
+
 			for (int columna : fila) {
-				
+
 				System.out.println(columna + " ");
 
 			}
 		}
-		
+
 	}
 
 }
