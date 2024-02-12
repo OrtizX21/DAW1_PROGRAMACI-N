637fe24ad77d86e6789faf80a596f20bea8dec86
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-02-12 16:07:06 +0100

    Add files via upload

---
+++ Character/Apuntes_Unicode.java
@@ -0,0 +1,32 @@
+package Character;
+
+public class Apuntes_Unicode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		char Corazon;
+		
+		Corazon = 9825;
+		
+		System.out.println(Corazon);
+		
+		char Secuencias;
+		
+		Secuencias = '\'';
+		
+		// MUETSRA COMILLA SIMPLE
+		System.out.println(Secuencias);
+		
+		// MUETSRA COMILLA DOBLE
+		Secuencias = '\"';
+		System.out.println(Secuencias);
+		
+		Secuencias = '\t';
+		
+		// TABULADOR
+		System.out.println("1" + Secuencias + "2");
+	}
+
+}
