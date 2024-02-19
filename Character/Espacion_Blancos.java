9531909ef9054d9b0d002fa506cfb624c1c128ef
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-02-19 14:42:20 +0100

    Update Espacion_Blancos.java

--- Character/Espacion_Blancos.java
+++ Character/Espacion_Blancos.java
@@ -3,14 +3,9 @@ package Character;
 import java.util.*;
 
 public class Espacion_Blancos {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		/*
-		 * Diseñar una aplicación que pida al usuario que introduzca una frase por
-		 * teclado e indique cuántos espacios en blanco tiene.
-		 */
+	
+	static void Espacios () {
+		
 		System.out.println("Introduce Una Cadena De Texto:");
 
 		Scanner Cadena = new Scanner(System.in);
@@ -33,6 +28,18 @@ public class Espacion_Blancos {
 		}
 
 		Cadena.close();
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * Diseñar una aplicación que pida al usuario que introduzca una frase por
+		 * teclado e indique cuántos espacios en blanco tiene.
+		 */
+		
+		Espacios();
 
 	}
 
