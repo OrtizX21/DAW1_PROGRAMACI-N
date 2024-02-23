8274f5f580317370437fd13b506fa27ffb853919
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-02-23 16:14:48 +0100

    Update Llamada_Objetos.java

--- POO/Llamada_Objetos.java
+++ POO/Llamada_Objetos.java
@@ -6,7 +6,9 @@ public class Llamada_Objetos {
 
 		Cuenta_Corriente CuentaAhorro;
 
-		CuentaAhorro = new Cuenta_Corriente("AX345621", "Felipe Ortiz", "BBVA"); // HAGO LA LLAMADA AL CONSTRUCTOR PADRE.
+		CuentaAhorro = new Cuenta_Corriente("AX345621", "Felipe Ortiz"); // HAGO LA LLAMADA AL CONSTRUCTOR PADRE.
+
+		CuentaAhorro.getBanco();
 
 		CuentaAhorro.Datos();
 
@@ -15,18 +17,21 @@ public class Llamada_Objetos {
 		// AMBOS SE VEN POR QUE UNO ESTA PUBLICO Y SE PUEDE VER DESDE OTRO PAQUETE AFUERA (DNI) Y (NOMBRED) QUE NO TIENE NINGUNA RESTRICCION
 		// Y SOLO SE VE EN EL PAQUETE.
 		
-		CuentaAhorro.Dni = "ADWDW12123";
-		CuentaAhorro.NombreD = "Mario Alfonso";
+		CuentaAhorro.Dni = "AM12343";
+		CuentaAhorro.NombreD = "USUARIO EJEMPLO";
 		
 		// NO SE VE POR QUE ESTA EN PRIVATE 
 		// CuentaAhorro.Saldo_Incial;
 		
 		Cuenta_Corriente CuentaPensionado;
 		
-		CuentaPensionado = new Cuenta_Corriente("AV125458", "Laura Rodriguez", 112154.02, ""); // HAGO LA LLAMADA AL CONSTRUCTOR SOBRECARGADO.
+		CuentaPensionado = new Cuenta_Corriente("AV125458", "Laura Rodriguez", 112154.02, "Caixa"); // HAGO LA LLAMADA AL CONSTRUCTOR SOBRECARGADO.
 		
+		CuentaPensionado.getBanco();
+
 		CuentaPensionado.Datos1();
 		
-
+		CuentaPensionado.setBanco("Santander");
+		
 	}
 }
