87627fe946bfa2d693765a291051eac76fe21742
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-02-26 17:31:50 +0100

    Update Llamada_Objetos.java

--- POO/Llamada_Objetos.java
+++ POO/Llamada_Objetos.java
@@ -1,33 +1,29 @@
 package POO;
 
 public class Llamada_Objetos {
-	
+
 	public static void main(String[] args) {
-		
+
 		Cuenta_Corriente CuentaAhorro;
-		
+
 		CuentaAhorro = new Cuenta_Corriente("AX345621", "Felipe Ortiz"); // HAGO LA LLAMADA AL CONSTRUCTOR PADRE.
-		
 
 		CuentaAhorro.setBanco("Santander");
-		
+
 		CuentaAhorro.getBanco();
-		
+
 		CuentaAhorro.Datos();
-		
+
 		CuentaAhorro.getSaldo(8000);
-		
-		
-		
+
 		// AMBOS SE VEN POR QUE UNO ESTA PUBLICO Y SE PUEDE VER DESDE OTRO PAQUETE
 		// AFUERA (DNI) Y (NOMBRED) QUE NO TIENE NINGUNA RESTRICCION
 		// Y SOLO SE VE EN EL PAQUETE.
-		
+
 		CuentaAhorro.Dni = "AM12343";
-		
+
 		CuentaAhorro.NombreD = "USUARIO EJEMPLO";
-		
-		
+
 		// NO SE VE POR QUE ESTA EN PRIVATE
 		// CuentaAhorro.Saldo_Incial;
 		// SOBRECARGA DEL OBJETO CUENTA DE BANCO.
