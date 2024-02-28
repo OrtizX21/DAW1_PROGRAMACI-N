fc95d2068c22a9036342c32756163ab25ea9c011
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-02-28 17:34:19 +0100

    Update Main_Gestor.java

--- POO/Main_Gestor.java
+++ POO/Main_Gestor.java
@@ -5,7 +5,7 @@ public class Main_Gestor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Cuenta_Corriente C1, C2;
+		Cuenta_Corriente C1, C2, C3;
 
 		Gestor_Cuenta G1 = new Gestor_Cuenta("Jose Luis", "+34 546 78 96 12");
 		Gestor_Cuenta G2 = new Gestor_Cuenta("Maria Jose", "+34 546 78 96 12");
@@ -14,21 +14,26 @@ public class Main_Gestor {
 
 		C2 = new Cuenta_Corriente("AZ234367", "Julian Dominguez", G2);
 
-		//C3 = new Cuenta_Corriente("AR234367", "Laura Rojas");
+		C3 = new Cuenta_Corriente("AR234367", "Laura Ochoa");
 
-		C1.DatosConGestor();
+
+		C1.informacionG();
 
 		System.out.println();
 
-		C2.DatosConGestor();
+		C2.informacionG();
 		
 		System.out.println();
 
-		//C3.DatosConGestor();
+		C3.informacionG();
+		
+		System.out.println();
 		
 		C1.setGestor(G2);
+		
+		System.out.println();
 
-		C1.DatosConGestor();
+		C1.informacionG();
 
 	}
 
