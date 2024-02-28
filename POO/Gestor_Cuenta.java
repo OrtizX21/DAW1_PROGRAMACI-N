e84a59a265d37a138d948b6faab39aedb9001c23
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-02-28 17:34:36 +0100

    Update Gestor_Cuenta.java

--- POO/Gestor_Cuenta.java
+++ POO/Gestor_Cuenta.java
@@ -5,21 +5,33 @@ public class Gestor_Cuenta {
 
 	public String NombGestor; // NOMBRE DEL GESTOR PREDETERMINADO.
 
-	String TelGestor; // NUMERO DE TELEFONO DEL GESTOR.
+	private String TelGestor; // NUMERO DE TELEFONO DEL GESTOR.
 
 	double ImporteMax = 0; // IMPORTE MAXIMO DE LA CUENTA.
 	
 
-	Gestor_Cuenta(String Nom, String Tel) {
+	public Gestor_Cuenta(String Nom, String Tel, double importe) {
 
 		this.NombGestor = Nom;
 		this.TelGestor = Tel;
-		this.ImporteMax = 10000;
+		this.ImporteMax = importe;
 
 	}
 	
+	
+	public Gestor_Cuenta (String Nom, String Tel) {
+		
+		this (Nom, Tel, 100000.);
+	}
+	
+	
+	String getTel () {
+		
+		return TelGestor;
+	}
+	
 
-	public void DatosGestor() {
+	public void DatosG() {
 
 		
 		System.out.println("El Nombre Del Gestor De Tu Cuenta Es: " + NombGestor);
