7210198fa1aa8920f93f7d879f6337c6e31057e3
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-02-29 15:38:37 +0100

    Update Hora.java

--- POO/Hora.java
+++ POO/Hora.java
@@ -14,46 +14,92 @@ public class Hora {
 	private int Seg;
 
 	private int A;
-	
 
 	Hora(int Hor, int Min, int Seg) { // CONSTRUCTOR PADRE.
 
+		/*
+		 * if (Seg <= 0 || Seg <= 58) {
+		 * 
+		 * Seg ++; }
+		 */
+		
+		
+		if (Min == 60 && Seg == 60) {
+			
+			Hor ++;
+			Min = 00;
+			Seg = 00;
+		}
+
+
+		if (Hor == 24) {
+
+			Hor = 0;
+		}
+
+		
 		this.Hor = Hor;
 		this.Min = Min;
 		this.Seg = Seg;
+		
+		
 	}
-	
+
 	void setHor(int Hor) {
 
+		if (Hor <= 0 && Hor <= 23) {
 		this.Hor = Hor;
+		}
+		
+		else {
+			
+			this.Hor = 0;
+		}
 	}
 
 	public int getHor() {
 
 		System.out.println(Hor);
+
 		return Hor;
 	}
 
 	void setMin(int Min) {
 
+		if (Min <= 0 && Min <= 59) {
 		this.Min = Min;
+		}
+		
+		else {
+			
+			this.Min = 0;
+		}
 
 	}
 
 	public int getMin() {
-
+	
 		System.out.println(Min);
+
 		return Min;
 	}
 
 	void setSeg(int Seg) {
 
+		if (Seg <= 0 && Seg <= 59) {
 		this.Seg = Seg;
+		}
+		
+		else {
+			
+			this.Seg = 0;
+		}
 	}
 
 	public int getSeg() {
 
 		System.out.println(Seg);
+
 		return Seg;
 	}
 
@@ -67,12 +113,22 @@ public class Hora {
 	public int getA() {
 
 		System.out.println();
+		
 		return A;
 	}
 
 	void MostrarInformacion() {
 
-		System.out.println("La Hora Es: " + Hor + " " + "Minutos: " + Min + " " + "Segundos: " + Seg);
+		if (Hor >= 24 || Min >= 60 || Seg >= 61) {
+			
+			System.out.println("Operacion Invalida");
+		}
+		
+		else {
+			
+			System.out.println("La Hora Es: " + Hor + " " + "Minutos: " + Min + " " + "Segundos: " + Seg);
+			
+		}
 
 	}
 
