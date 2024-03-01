d878004e5c6ad04da1a934fb5c970b8ea5b4c941
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-03-01 18:00:51 +0100

    Update Main_Comic.java

--- POO/Main_Comic.java
+++ POO/Main_Comic.java
@@ -5,18 +5,35 @@ public class Main_Comic {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Comic Comic1;
+		Comic Comic1, Comic2, Comic3;
 		
-		Comic1 = new Comic ("Hajime No Ippo", 1078, 20, 4);
+		Comic1 = new Comic ("Hajime No Ippo", 179, 20, 4);
 		
-		Comic1.Prestamo(5);
+		Comic1.Prestamo();
 		Comic1.Info();
 		
 		System.out.println();
 		
-		Comic1.Devolucion(3);
+		Comic1.Devolucion();
 		Comic1.Info();
 		
+		System.out.println();
+		
+		//////////////////////////////////////////////////////////////////
+		
+		Comic2 = new Comic ("Ashita No Joe", 180, 10, 9);
+		
+		Comic2.Prestamo();
+		Comic2.Info();
+		
+		System.out.println();
+		
+		//////////////////////////////////////////////////////////////////
+
+		Comic3 = new Comic ("One Piece", 181, 1, 0 );
+		
+		Comic3.Prestamo();
+		Comic3.Info();
 
 	}
 
