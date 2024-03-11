f7b97c57157c8ceb353bc88e3d6699014ce337e1
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-03-11 16:02:32 +0100

    Update Examen2.java

--- POO/Examen2.java
+++ POO/Examen2.java
@@ -1,4 +1,4 @@
-package Pruebas;
+package POO;
 import java.util.*;
 
 public class Examen2 {
@@ -12,7 +12,7 @@ public class Examen2 {
 		// 2 INICIALIZAR LA TABLA CON VALORES MAYORES A -100.
 		for (int i = 0; i < t.length; i++) {
 
-			t[i] = (int) ((Math.random() * -100) + 200);
+			t[i] = (int) ((Math.random() * 200) -100);
 		}
 
 		// 3 MOSTRAR LA SUMA DE TODOS LOS VALORES DE LA TABLA.
@@ -28,6 +28,26 @@ public class Examen2 {
 		
 		//4 BORRAR LOS ELEMENTOS QUE SEAN 20 DE LA TABLA.
 		
+		int tcopia [] = Arrays.copyOf(t, t.length);
+		int i =  0;
+		
+		while (i < tcopia.length) {
+			
+			if (tcopia[i] == 20) {
+				tcopia[i] = tcopia [tcopia.length-1];
+				tcopia = Arrays.copyOf(tcopia, tcopia.length-1);
+			}
+			
+			else {
+				
+				i++;
+			}
+			
+			t = tcopia;
+			
+			Arrays.sort(t);
+			System.out.println(Arrays.toString(t));
+		}
 		
 		//5 INSERTAR EL NUMERO 28 EN LA TABLA T.
 		t = Arrays.copyOf(t, t.length +1);
@@ -37,23 +57,23 @@ public class Examen2 {
 		//6 MOSTRAR LA TABLA Y TRATARLA DE MANERA ORDENADA.
 		Arrays.sort(t);
 		
-		//7 INSTWRTAR EL NUMERO 35 EN LA TABLA.
-		int [] aux1= new int [t.length+1];
+		//7 INSTERTAR EL NUMERO 35 EN LA TABLA.
+		int [] copia= new int [t.length+1];
 		int indice = Arrays.binarySearch(t, 35);
 		
 		if (indice>=0) { //Significa que el numero lo ha encontrado en la tabla
-			System.arraycopy(t, 0, aux1, 0, indice);
-			System.arraycopy(t, indice, aux1, indice+1, t.length-indice);
-			aux1[indice]=35;
+			System.arraycopy(t, 0, copia, 0, indice);
+			System.arraycopy(t, indice, copia, indice+1, t.length-indice);
+			copia[indice]=35;
 			
 		} else { // No se encuentra en la tabla
 			indice=-indice-1;
-			System.arraycopy(t, 0, aux1, 0, indice);
-			System.arraycopy(t, indice, aux1, indice+1, t.length-indice);
-			aux1[indice]=35;
+			System.arraycopy(t, 0, copia, 0, indice);
+			System.arraycopy(t, indice, copia, indice+1, t.length-indice);
+			copia[indice]=35;
 		}
 		
-		t=Arrays.copyOf(aux1, aux1.length);
+		t=Arrays.copyOf(copia, copia.length);
 		
 		//8 MOSTRAR LA CANTIDAD DE REGISTROS QUE TIENE LA CUENTA. 
 		int Registros = t.length;
