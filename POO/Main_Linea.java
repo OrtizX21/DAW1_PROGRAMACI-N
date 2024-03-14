a36e12fc69b5c7bca79b4b6a2401237b9c3c57d2
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-03-15 00:26:15 +0100

    Update Main_Linea.java

--- POO/Main_Linea.java
+++ POO/Main_Linea.java
@@ -2,4 +2,17 @@ package POO;
 
 public class Main_Linea {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Punto Punto1 = new Punto (0, 0);
+		Punto Punto2 = new Punto (1, 1);
+
+		Linea MiLinea = new Linea (Punto1, Punto2);
+		System.out.println(MiLinea);
+		
+
+	}
+
 }
