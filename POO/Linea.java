feb3f8fabf6620c33f3df893e638a330c40e7e28
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-03-15 00:25:55 +0100

    Update Linea.java

--- POO/Linea.java
+++ POO/Linea.java
@@ -1,22 +1,62 @@
-
 package POO;
 
 public class Linea {
 
-	Punto puntoA; 
-	Punto PuntoB;
+	private Punto PuntoA;
+	private Punto PuntoB;
 	
-	// CONSTRUCTOR LINEA QUE RECIBE DOS OBJETOS DE TIPO PUNTO.
-	
-	public Linea (Punto Punto1 ,Punto Punto2) {
-  
-	this.puntoA = Punto1; 
-	this.PuntoB = Punto2;
-	
+	public Linea (Punto Punto1, Punto Punto2) {
+		
+		this.PuntoA = Punto1;
+		this.PuntoB = Punto2;	
 	}
 	
-	double mueveDerecha (double x) {
+	@Override
+	public String toString() {
+		return "Linea [PuntoA = " + PuntoA + ", PuntoB = " + PuntoB + "]";
+	}
+	
+	double mueveDerecha (double xPositivo) {
 		
-		return x;
+		this.PuntoA.x += xPositivo;
+		this.PuntoB.x += xPositivo;
+		return xPositivo;
+	}
+	
+	double mueveIzquierda (double xNegativo) {
+		
+		this.PuntoA.x -= xNegativo;
+		this.PuntoB.x -= xNegativo;
+		return xNegativo;
+	}
+	
+	double mueveArriba (double yPositivo) {
+		
+		this.PuntoA.y += yPositivo;
+		this.PuntoB.y += yPositivo;
+		return yPositivo;
+	}
+	
+	double mueveAbajo (double yNegativo) {
+		
+		this.PuntoA.y -= yNegativo;
+		this.PuntoB.y -= yNegativo;
+		return yNegativo;
+	}	
+	
+	boolean lineasParalelas (Linea uno, Linea dos) {
+		
+		boolean Paralela = false;
+		
+		double forma1 = (uno.PuntoA.y - uno.PuntoB.y);
+		double forma2 = (dos.PuntoA.y - dos.PuntoB.y);
+		
+		if (forma1 == forma2) {
+			
+			Paralela = true;
+			System.out.println("Las Lineas Son Paralelas: " + Paralela);
+		}
+		
+		return Paralela;
 	}
 }
