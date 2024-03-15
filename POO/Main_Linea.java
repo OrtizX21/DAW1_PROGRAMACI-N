e9b4162a3355bc28916d47b2e74babf649844645
Felipe Ortiz <148568461+OrtizX21@users.noreply.github.com>
2024-03-15 16:07:25 +0100

    Update Main_Linea.java

--- POO/Main_Linea.java
+++ POO/Main_Linea.java
@@ -5,14 +5,39 @@ public class Main_Linea {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// CREO Y MUESTRO TODOS LOS PUNTOS CON EL METODO TO STRING.
+		Punto Punto1 = new Punto (0.0, 5.0);
+		System.out.println(Punto1);
 		
-		Punto Punto1 = new Punto (0, 0);
-		Punto Punto2 = new Punto (1, 1);
-
-		Linea MiLinea = new Linea (Punto1, Punto2);
-		System.out.println(MiLinea);
+		Punto Punto2 = new Punto (1.0, 1.0);
+		System.out.println(Punto2 + "\n");
 		
+		Punto Punto3 = new Punto (0.0, 5.0);
+		System.out.println(Punto3);
+		
+		Punto Punto4 = new Punto (1.0, 1.0);
+		System.out.println(Punto4);
 
+		System.out.println();
+
+		// CREO Y MUESTRO TODOS LAS LINEAS CON EL METODO TO STRING.
+		Linea MiLinea1 = new Linea (Punto1, Punto2);
+		Linea MiLinea2 = new Linea (Punto3, Punto4);
+		
+		System.out.println(MiLinea1 + "\n");
+		System.out.println(MiLinea2 + "\n");
+		
+		// COMPRUEBO SI LAS LINEAS SON PARALELAS CON EL METODO LINEAS PARALELAS DE LA CLASE PUNTO.
+		boolean Paralelas = Linea.lineasParalelas(MiLinea1, MiLinea2);
+		
+		if (Paralelas == true) {
+			
+			System.out.println("Las Lineas Son Paralelas: " + Paralelas + ".");
+		}
+		
+		else {
+			Paralelas = false;
+			System.out.println("Las Lineas No Son Paralelas: " + Paralelas + ".");
+		}		
 	}
-
 }
